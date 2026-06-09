5e7f739e9f1cc3908218c7c79d253311d4179fd5
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-06-09 19:02:12 +0530

    add question set of basic type

---
+++ basic/questionOfbasic.js
@@ -0,0 +1,28 @@
+// 1. Find all users from Delhi.
+// 2. Find users whose age is greater than 25
+// 3. Find users whose age is less than or equal to 30.
+// 4. Find users whose city is not Mumbai.
+// 5. Find users whose age is between 20 and 30.
+// 6. Show only name and email of all users.
+// 7. Find users whose name starts with "R".
+// 8. Find users whose email contains gmail.
+// 9. Find users whose city field exists.
+// 10. Find users whose phone field does not exist.
+
+// 11. Sort users by age ascending.
+// 12. Sort users by age descending.
+// 13. Show only first 5 users.
+// 14. Skip first 10 users and show next 5 users.
+// 15. Find users whose age is either 20, 25, or 30.
+// 16. Find users whose age is not 20, 25, 30.
+// 17. Count total users in database.
+// 18. Find users whose name is Rahul Sharma and city is Delhi.
+// 19. Find users whose city is Delhi OR Mumbai.
+// 20. Find users whose age is not greater than 30.
+
+
+// 21. Find users whose name ends with "a".
+// 22. Find users whose email domain is yahoo.com.
+// 23. Find users whose age field type is number.
+// 24. Find users where age is even.
+// 25. Find users whose name length is greater than 5 characters.
